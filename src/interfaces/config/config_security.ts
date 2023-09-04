048def97bf3d3c84dc758fe9fcc8055f5756216b
King Richard Gutierrez <dev.krfgutierrez@gmail.com>
2023-09-05 06:23:44 +0800

    refactor: update the forgot of jwt config
    Instead of having separate secret for access token and refresh token, use the same secret.

--- src/interfaces/config/config_security.ts
+++ src/interfaces/config/config_security.ts
@@ -9,11 +9,11 @@ export interface IConfigSecurityPassword {
 }
 
 export interface IConfigSecurityAuthentication {
+  jwt_secret: string;
   access_token: IConfigSecurityAuthenticationToken;
   refresh_token: IConfigSecurityAuthenticationToken;
 }
 
 export interface IConfigSecurityAuthenticationToken {
   duration: string;
-  secret: string;
 }
